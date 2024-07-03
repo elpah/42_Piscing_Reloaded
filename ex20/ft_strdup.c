f6a5271a275673b00e995a67ef0f8ee1095edf25
Elpah <obengelpachris@gmail.com>
2024-07-03 21:11:22 +0200

    added ex20 git push

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eobeng <eobeng@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 14:00:54 by eobeng            #+#    #+#             */
+/*   Updated: 2024/06/06 16:14:06 by eobeng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		j;
+	char	*dest;
+
+	i = 0;
+	j = ft_strlen(src) + 1;
+	dest = malloc(sizeof(char) * j);
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
